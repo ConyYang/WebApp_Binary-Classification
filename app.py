e50fc446a9c2529f5104d0e94e720d095bd6b0d1
Yang Yubei <yangyubei0218@gmail.com>
2020-06-02 16:15:04 +0800

    3 classifiers done

--- app.py
+++ app.py
@@ -8,6 +8,9 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import plot_confusion_matrix, plot_roc_curve, plot_precision_recall_curve
 from sklearn.metrics import precision_recall_curve, precision_score, recall_score
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+
 
 def main():
     st.title('Binary Classification Web App')
@@ -15,7 +18,7 @@ def main():
     st.markdown('Are your mushrooms edible or poisonous? 🐷')
     st.sidebar.markdown('Are your mushrooms edible or poisonous? 😃')
 
-    @st.cache(persist=True)# cache the output to disk
+    @st.cache(persist=True)  # cache the output to disk
     def load_data():
         data = pd.read_csv('mushrooms.csv')
         # instantiate helper function
@@ -60,24 +63,89 @@ def main():
 
     if classifier == 'Super Vector Machine (SVM)':
         st.sidebar.subheader("Model Hyperparameters")
-        C = st.sidebar.number_input("C (Regularization parameter)", 0.01, 10.0, steps=0.01, key = 'C')
-        kernel = st.sidebar.radio("kernel", ("rbf", "linear"), key='kernel')
+        c = st.sidebar.number_input("C (Regularization parameter 0.01-10.0)", 0.01, 10.0, step=0.01, key='C')
+        kernel = st.sidebar.radio("Kernel", ("rbf", "linear"), key='kernel')
+        gamma = st.sidebar.radio("Gamma (kernel Coefficient)", ("scale", "auto"), key="gamma")
 
+        metrics = st.sidebar.multiselect("What metrics to plot?",
+                                         ('Confusion Matrix', 'ROC Curve', 'Precision-Recall Curve'))
 
+        # Train the model and print the results
+        if st.sidebar.button('Classify', key='classify'):
+            st.subheader("Support Vector Machine (SVM) Results")
+            model = SVC(C=c, kernel=kernel, gamma=gamma)
+            model.fit(x1, y1)
+            # model = make_pipeline(StandardScaler(), SVC(C=c, kernel=kernel, gamma=gamma))
+            # model.fit(x1, y1)
+            accuracy = model.score(x2, y2)
+            y_pred = model.predict(x2)
+            precision_score_xy = precision_score(y2, y_pred, labels=class_names)
+            recall_score_xy = recall_score(y2, y_pred, labels=class_names)
 
+            st.write("Accuracy: ", round(accuracy, 2))
+            st.write("Precision: ", round(precision_score_xy, 2))
+            st.write("Recall: ", round(recall_score_xy, 2))
 
+            plot_metrics(metrics)
 
+    if classifier == 'Logistic Regression':
+        st.sidebar.subheader("Model Hyperparameters")
+        c_lr = st.sidebar.number_input("C (Regularization parameter 0.01-10.0)", 0.01, 10.0, step=0.01, key='C_lr')
+        max_iter = st.sidebar.slider("Maximum number of iterations", 100, 500, key='max_iter')
 
+        metrics = st.sidebar.multiselect("What metrics to plot?",
+                                         ('Confusion Matrix', 'ROC Curve', 'Precision-Recall Curve'))
 
+        # Train the model and print the results
+        if st.sidebar.button('Classify', key='classify'):
+            st.subheader("Logistic Regression Results")
+            model = LogisticRegression(C=c_lr, max_iter=max_iter)
+            model.fit(x1, y1)
+            accuracy = model.score(x2, y2)
+            y_pred = model.predict(x2)
+            precision_score_xy = precision_score(y2, y_pred, labels=class_names)
+            recall_score_xy = recall_score(y2, y_pred, labels=class_names)
 
+            st.write("Accuracy: ", round(accuracy, 2))
+            st.write("Precision: ", round(precision_score_xy, 2))
+            st.write("Recall: ", round(recall_score_xy, 2))
 
+            plot_metrics(metrics)
 
+    if classifier == 'Random Forest':
+        st.sidebar.subheader("Model Hyperparameters")
+        n_estimators = st.sidebar.number_input("The number of trees in the forest (100-5000)",
+                                               100, 5000, step=100, key='n_estimators')
+        max_depth = st.sidebar.number_input("The maximum depth of the tree (1-20)",
+                                            1, 20,step=1, key='max_depth')
+        bootstrap = st.sidebar.radio("Bootstrap samples when building trees",
+                                     ('True', 'False'), key='bootstrap')
+        metrics = st.sidebar.multiselect("What metrics to plot?",
+                                         ('Confusion Matrix', 'ROC Curve', 'Precision-Recall Curve'))
 
+        # Train the model and print the results
+        if st.sidebar.button('Classify', key='classify'):
+            st.subheader("Random Forest Results")
+            model = RandomForestClassifier(n_estimators=n_estimators,
+                                           bootstrap=bootstrap,
+                                           max_depth=max_depth,
+                                           n_jobs=-1)
+            model.fit(x1, y1)
+            accuracy = model.score(x2, y2)
+            y_pred = model.predict(x2)
+            precision_score_xy = precision_score(y2, y_pred, labels=class_names)
+            recall_score_xy = recall_score(y2, y_pred, labels=class_names)
 
+            st.write("Accuracy: ", round(accuracy, 2))
+            st.write("Precision: ", round(precision_score_xy, 2))
+            st.write("Recall: ", round(recall_score_xy, 2))
+
+            plot_metrics(metrics)
     # set the default state of Checkbar to be unchecked
     if st.sidebar.checkbox("Show raw data", False):
         st.subheader("Mushroom Data Set (Classification)")
         st.write(df)
+
 
 if __name__ == '__main__':
     main()
